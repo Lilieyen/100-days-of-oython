a7f24ef525ca18b42f724d68b6884060ef76a728
lilieyen <lnyasita@gmail.com>
2022-10-15 21:50:43 +0300

    Day 24 project

---
+++ day24/main.py
@@ -0,0 +1,6 @@
+with open("../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# with open("new_file.txt", mode="w") as file:
+#    file.write("This is the day the Lord has made, I will rejoice and be glad in it!")
